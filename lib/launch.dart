de0cd1447b5fe6a4b9459a617fb35f9f3d75777c
谢雄 <76127031@qq.com>
2026-01-27 16:41:13 +0800

    ♻️ update: 数字人二期

---
+++ lib/launch.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LaunchPage extends StatefulWidget {
+  const LaunchPage({super.key});
+
+  @override
+  State<LaunchPage> createState() => _LaunchPageState();
+}
+
+class _LaunchPageState extends State<LaunchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/router.dart
+++ lib/router.dart
@@ -1,11 +1,23 @@
+/*
+ * 文件名称: router.dart
+ * 创建时间: 2026/01/22 15:18:47
+ * 作者名称: Andy.Zhao
+ * 联系方式: smallsevenk@vip.qq.com
+ * 创作版权: Copyright (c) 2026 XianHua Zhao (andy)
+ * 功能描述: 注册路由
+ */
+
 import 'package:xmca/xmca.dart';
 import 'package:xmcp_digital/xmcp_digital.dart';
 import 'package:xmcs/xmcs.dart';
+import 'package:xmcs_module/launch.dart';
 import 'package:xmcs_module/main.dart';
 
+const String launch = "/xmlaunch";
 registRouters() {
   XRouter.instance.registRouters([
     GoRoute(path: XRouter.instance.initialLocation, builder: (context, state) => HomePage()),
+    GoRoute(path: launch, builder: (context, state) => LaunchPage()),
   ]);
   // 注册xmcs相关路由
   registCsRouters();
